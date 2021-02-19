df43b2b8c28b03693c2051ded64326f5358fba31
Bryan <bryanblack1999@gmail.com>
2021-02-18 19:43:18 -0600

    serviceCard add

---
+++ src/Components/InfoHoverCard.jsx
@@ -0,0 +1,135 @@
+import React, {useState} from 'react'
+import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+const InfoHoverCard = ( {img,title,description,logo} ) => {
+
+  const [ hovercard, setHovercard ] = useState(false);
+  const [ dinamicstyles, setDinamicstyles ] = useState({
+    filter: 'grayscale(100%)',
+    height: '300px',
+    opacity: '1',
+  })
+
+  const handleHoverActive = () => {
+    if(!hovercard){
+      setHovercard(true)
+      setDinamicstyles({
+        ... dinamicstyles,
+        filter: 'grayscale(0%)',
+        height: '350px',
+        opacity: '0.6',
+      })
+
+    }else{
+      setHovercard(false)
+      setDinamicstyles({
+        ... dinamicstyles,
+        filter: 'grayscale(100%)',
+        height: '300px',
+        opacity: '1',
+      })
+    }
+  }
+
+  const useStyles = makeStyles( (theme) => ({
+    card: {
+      borderTop: '20px solid #00A8BA',
+      borderRadius: '20px',
+      overflow: 'hidden',
+      boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+      postion: 'relative',
+      transition: 'all 300ms ease',
+      backgroundColor: 'black'
+    },
+    title: {
+      fontWeight: 'bold'
+    },
+    media: {
+      height: `${dinamicstyles.height}`,
+      filter: `${dinamicstyles.filter}`,
+      opacity: `${dinamicstyles.opacity}`,
+      
+    },
+    detailsContainer: {
+      backgroundColor: 'transparent',
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      right: '0',
+      bottom: '0',
+      color: 'white',
+    },
+    logoContainer: {
+      width: '90px',
+      height: '90px',
+      backgroundColor: '#F0F0F0',
+      border: '5px solid #C4C4C4',
+      borderRadius: '50%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      '& img': {
+        width: '80%',
+        height: '80%',
+      }
+    },
+    bg: {
+      backgroundColor: 'white'
+    }
+  }));
+
+  const classes = useStyles();
+
+  return(
+    
+    <Card 
+    className={classes.card} 
+    onMouseEnter={() => (handleHoverActive())} 
+    onMouseLeave={() => (handleHoverActive())}
+    >
+      { !hovercard ? 
+        <CardActionArea >
+          <CardMedia
+            className={classes.media}
+            image={img}
+          />
+          <CardContent className={classes.bg}>
+            <Typography variant="h5" component="h2" align="center">
+              {title}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        :
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={img}
+          />
+          <CardContent
+          align="center"
+          className={classes.detailsContainer}>
+            <Box
+            className={classes.logoContainer}>
+              <img src={logo} alt="logo servicio"/>
+            </Box>
+            <Typography className={classes.title} variant="h5" component="h2" align="center">
+              {title}
+            </Typography>
+            <Typography component="h2" align="center">
+              {description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        
+      }
+    </Card>
+  )
+
+}
+
+export default InfoHoverCard;
--- src/Pages/Hardware.jsx
+++ src/Pages/Hardware.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
-
+import { Container, Grid } from '@material-ui/core';
+import InfoHoverCard from '../Components/InfoHoverCard';
+import informaticaForenceImg from '../Assets/imgs/informaticaForenceCard.png';
+import informaticaForenceImgLogo from '../Assets/imgs/informaticaForenceCardLogo.png';
 export default function Hardware() {
     return (
         <Container maxWidth="xl">
-
+          <Grid xs='3'>
+            <InfoHoverCard
+            img={informaticaForenceImg}
+            logo={informaticaForenceImgLogo}
+            title='Informatica Forence'
+            description='Brindamos soluciones dedicadas a la Seguridad de la información y Peritaje Informático Forense ofreciendo alternativas preventivas y correctivas para salvaguarda las propiedades digitales y la información valiosa de su negocio.'
+            />
+          </Grid>
         </Container>
     )
 }
